1df3aa8230a8c80c294d8e076c60804cd67323cb
n20va <99407852+n20va@users.noreply.github.com>
2026-05-12 19:21:06 +0300

    Delete core/user-service directory

--- core/user-service/src/main/java/ru/practicum/ewm/user/dto/UserSearchRequest.java
+++
@@ -1,3 +0,0 @@
-package ru.practicum.ewm.user.dto;
-import java.util.List;
-public record UserSearchRequest(List<Long> ids, Integer from, Integer size) {}
--- core/user-service/src/main/java/ru/practicum/ewm/user/exception/ConflictException.java
+++
@@ -1,2 +0,0 @@
-package ru.practicum.ewm.user.exception;
-public class ConflictException extends RuntimeException { public ConflictException(String m){super(m);} }
