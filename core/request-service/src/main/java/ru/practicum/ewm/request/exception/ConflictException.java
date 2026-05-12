dbf631e608db8f208b96bee19959be8f290abf36
n20va <99407852+n20va@users.noreply.github.com>
2026-05-12 19:20:57 +0300

    Delete core/request-service directory

--- core/request-service/src/main/java/ru/practicum/ewm/request/exception/ConflictException.java
+++
@@ -1,4 +0,0 @@
-package ru.practicum.ewm.request.exception;
-public class ConflictException extends RuntimeException {
-    public ConflictException(String m) { super(m); }
-}
--- core/request-service/src/main/java/ru/practicum/ewm/request/model/RequestStatus.java
+++
@@ -1,2 +0,0 @@
-package ru.practicum.ewm.request.model;
-public enum RequestStatus { PENDING, CONFIRMED, REJECTED, CANCELED }
