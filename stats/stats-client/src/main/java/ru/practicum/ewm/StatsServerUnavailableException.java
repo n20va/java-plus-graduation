69c1dc49e3a1d66d7d87c25b1185f2860441f804
n20va <99407852+n20va@users.noreply.github.com>
2026-05-12 18:45:13 +0300

    Delete stats directory

--- stats/stats-client/src/main/java/ru/practicum/ewm/StatsServerUnavailableException.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.ewm;
-
-public class StatsServerUnavailableException extends RuntimeException {
-
-    public StatsServerUnavailableException(String message) {
-        super(message);
-    }
-
-}
--- stats/stats-dto/src/main/java/ru/practicum/ewm/ViewStatsDto.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.ewm;
-
-public record ViewStatsDto(
-        String app,
-        String uri,
-        Long hits
-) {
-}
