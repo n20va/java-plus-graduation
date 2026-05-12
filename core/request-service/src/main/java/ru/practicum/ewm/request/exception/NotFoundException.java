393257dbf936ee8adfc2832e3bb6bc8fc0adce43
n20va <99407852+n20va@users.noreply.github.com>
2026-05-12 18:30:23 +0300

    Delete core/request-service directory

--- core/request-service/src/main/java/ru/practicum/ewm/request/exception/NotFoundException.java
+++
@@ -1,4 +0,0 @@
-package ru.practicum.ewm.request.exception;
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String m) { super(m); }
-}
--- core/request-service/src/main/java/ru/practicum/ewm/request/exception/ServiceUnavailableException.java
+++
@@ -1,4 +0,0 @@
-package ru.practicum.ewm.request.exception;
-public class ServiceUnavailableException extends RuntimeException {
-    public ServiceUnavailableException(String m) { super(m); }
-}
